2b777105917cfa870c0e8477f9ac99666f8d224b
umairniaziofficial <umairniaziofficial@gmail.com>
2024-04-28 19:59:44 +0500

    Routing Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div className="text-4xl">Code With HELLO</div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
 
-export default App
+export default App;
