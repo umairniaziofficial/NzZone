75660f9566e1f5a3c831a2da1ff307f92e2bb989
umairniaziofficial <umairniaziofficial@gmail.com>
2024-05-01 00:05:57 +0500

    App writer Id's Integrated

--- src/lib/appwriter/config.ts
+++ src/lib/appwriter/config.ts
@@ -3,6 +3,12 @@ import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 export const appwriterConfig = {
     projectId: import.meta.env.VITE_CONFIG_ID,
     url: import.meta.env.VITE_CONFIG_URL,
+    databaseID: import.meta.env.VITE_CONFIG_DATABASE_ID,
+    storageID: import.meta.env.VITE_CONFIG_STORAGE_ID,
+    usersCollectionID: import.meta.env.VITE_CONFIG_DATABASE_USERS,
+    postsCollectionID: import.meta.env.VITE_CONFIG_DATABASE_POSTS,
+    savesCollectionID: import.meta.env.VITE_CONFIG_DATABASE_SAVES,
+
 };
 export const client = new Client();
 
